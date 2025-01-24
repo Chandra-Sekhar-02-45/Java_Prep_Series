bed5a5b38bbdfa8b85727ce84d678bc60c00de5a
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-24 22:05:25 +0530

    completed the basic type conversion code in java

--- Type_Casting/src/typeCast.java
+++ Type_Casting/src/typeCast.java
@@ -4,5 +4,13 @@ class typeCast{
        double a = 27.67;
        int n = (int) a;
        System.out.println(n);
+
+       float b = (float) n;
+       System.out.println(b);
+
+        int a2=257;
+        byte b2=(byte) a2; // 257%256 = 1 ( range of byte is 256
+        System.out.println(b2);
+
     }
         }
