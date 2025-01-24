e41275c34c0068307f45820d7a7d18aca9a55daa
R.V.Chandra Sekhara Raju <rvcsraju@gmail.com>
2025-01-25 00:20:10 +0530

    Update userInputs.java

--- javaBasics/src/userInputs.java
+++ javaBasics/src/userInputs.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class userInputs{
     public static void main(String[] args) {
+        
         Scanner sc  = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = sc.nextLine();
