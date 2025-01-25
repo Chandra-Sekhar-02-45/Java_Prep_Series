511bd829828dc8a8c4bde0c07fb92a6e133cc633
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-25 19:48:25 +0530

    conditioanl statements are added

---
+++ javaBasics/src/conditionalStatements.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class conditionalStatements {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Age : ");
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are eligible for voting");
+        }
+        else {
+            System.out.println("You are not eligible for voting");
+        }
+    }
+}
