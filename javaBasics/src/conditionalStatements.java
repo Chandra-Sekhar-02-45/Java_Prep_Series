fb0858319c2f271044c2b3b136a4cdca6cfddfbf
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-01-25 19:54:04 +0530

    added elseif statement also

--- javaBasics/src/conditionalStatements.java
+++ javaBasics/src/conditionalStatements.java
@@ -7,8 +7,11 @@ public class conditionalStatements {
         System.out.println("Enter Your Age : ");
         int age = sc.nextInt();
 
-        if (age >= 18) {
+        if (age > 18) {
             System.out.println("You are eligible for voting");
+        }
+        else if (age == 18) {
+            System.out.println("You are still eligible for voting");
         }
         else {
             System.out.println("You are not eligible for voting");
