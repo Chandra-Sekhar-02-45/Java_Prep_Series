f32b3f5efd5829d23cb2cc391be95e85ae548293
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-02-26 11:49:10 +0530

    compound intrest calcular is completed

--- Java Projects/src/compIntrestCalc.java
+++ Java Projects/src/compIntrestCalc.java
@@ -5,5 +5,27 @@ public class compIntrestCalc {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
+        double principle, rate, amount;
+        int timesCompounded, years;
+
+        System.out.print("Enter the principal amount: ");
+        principle = sc.nextDouble();
+
+        System.out.print("Enter the rate of interest (in %): ");
+        rate = sc.nextDouble() / 100;
+
+        System.out.print("Enter the number of times interest is compounded per year: ");
+        timesCompounded = sc.nextInt();
+
+        System.out.print("Enter the number of years: ");
+        years = sc.nextInt();
+
+        amount = principle * Math.pow(1 + (rate / timesCompounded), timesCompounded * years);
+
+        // Format the output to 2 decimal places
+        System.out.printf("The amount after %d years is: $%.2f%n", years, amount);
+
+        sc.close();
     }
 }
