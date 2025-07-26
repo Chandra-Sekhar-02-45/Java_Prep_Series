e39d6b132f6b8f7157708d3fac57e8fc6c63808e
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-07-26 15:04:14 +0530

    Oops Concept Class is completed

--- Java Programs/src/Oops/Class/Car.java
+++ Java Programs/src/Oops/Class/Car.java
@@ -2,4 +2,12 @@ package Oops.Class;
 
 public class Car {
 
+    // Attributes (Fields / Properties)
+    String brand;
+    int speed;
+
+    // Behavior (Method)
+    void drive() {
+        System.out.println(brand + " is driving at " + speed + " km/h.");
+    }
 }
--- Java Programs/src/Oops/Class/Main.java
+++ Java Programs/src/Oops/Class/Main.java
@@ -1,5 +1,17 @@
 package Oops.Class;
 
-public class Main
-{
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();  // Creating an object of Car
+        car1.brand = "Toyota";
+        car1.speed = 120;
+
+        Car car2 = new Car();
+        car2.brand = "BMW";
+        car2.speed = 150;
+
+        car1.drive();  // Output: Toyota is driving at 120 km/h.
+        car2.drive();  // Output: BMW is driving at 150 km/h.
+    }
 }
+
