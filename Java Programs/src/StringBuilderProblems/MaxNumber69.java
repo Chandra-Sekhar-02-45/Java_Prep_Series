c649df82401291ba89fb6d3d0f303cdf989c1fb5
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-08-16 10:49:22 +0530

    commented

--- Java Programs/src/StringBuilderProblems/MaxNumber69.java
+++ Java Programs/src/StringBuilderProblems/MaxNumber69.java
@@ -1,3 +1,7 @@
+// LeetCode 1323 - Maximum 69 Number
+// Question: Given a positive integer num consisting only of digits 6 and 9,
+// return the maximum number you can get by changing at most one digit (6 becomes 9).
+
 package StringBuilderProblems;
 
 import java.util.Scanner;
@@ -13,7 +17,7 @@ public class MaxNumber69 {
                 return Integer.parseInt(sb.toString());
             }
         }
-        return num;
+        return num; // if no '6' is found
     }
 
     public static void main(String[] args) {
@@ -22,7 +26,7 @@ public class MaxNumber69 {
         System.out.println("Enter a number consisting of only digits 6 and 9:");
         int num = sc.nextInt();
 
-        // Direct static method call, no object needed
+        // Direct static method call (no object creation needed)
         int result = maximum69Number(num);
 
         System.out.println("Maximum number after flipping at most one '6' to '9' is: " + result);
