10d03dbf99393e67c1989c4916a23b8696ffa7f2
Chandra-Sekhar-02-45 <rvcsraju@gmail.com>
2025-08-24 16:43:00 +0530

    Reversing an array , in 3 approaches

--- Java Problems/src/ArraysProblems/reversingAnArray.java
+++ Java Problems/src/ArraysProblems/reversingAnArray.java
@@ -3,6 +3,13 @@ package ArraysProblems;
 import java.util.Scanner;
 
 public class reversingAnArray {
+
+    public static void swap(int[] arr , int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array : ");
@@ -20,15 +27,34 @@ public class reversingAnArray {
 //        }
 
         // Approach - 2
-        int temp;
-        for (int i = 0; i < n / 2 ; i++) {
-            temp = arr[i];
-            arr[i] = arr[n - i - 1];
-            arr[n - i - 1] = temp;
+//        int temp;
+//        for (int i = 0; i < n / 2 ; i++) {
+//            temp = arr[i];
+//            arr[i] = arr[n - i - 1];
+//            arr[n - i - 1] = temp;
+//        }
+//
+//        System.out.print("The reversed array is : ");
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(arr[i] + " ");
+//        }
+
+        // Approach - 3
+        int first = 0;
+        int last = n - 1;
+        for(int i = 0 ; i < n ; i++) {
+            if (first < last){
+                swap(arr , first , last);
+                first ++ ;
+                last -- ;
+            }
+            else {
+                break;
+            }
         }
 
-        System.out.print("The reversed array is : ");
-        for (int i = 0; i < n; i++) {
+        System.out.println("The reversed array is : ");
+        for(int i = 0 ; i < n ; i++){
             System.out.print(arr[i] + " ");
         }
     }
